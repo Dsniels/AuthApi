28d31aa752704d7e98be4f040b25ddbfa22e0e7b
Daniel Salazar <dsnile.salazr@gmail.com>
2024-05-04 16:36:27 -0600

    chore: Add config file with user roles, JWT secret, and MongoDB URL

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    userRoles : ['user', 'manager'],
+    jwtSecret : 'secreto',
+    mongoURL : 'mongodb+srv://dsnilesalazr:Dasa270512@cluster0.y2dzttg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+}
