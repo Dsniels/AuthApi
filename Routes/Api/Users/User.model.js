d9bff8daed337363559bc1489f1bdb4197d86e7f
Daniel Salazar <dsnile.salazr@gmail.com>
2024-05-04 16:26:06 -0600

    chore: Add required "service" field to User model

--- Routes/Api/Users/User.model.js
+++ Routes/Api/Users/User.model.js
@@ -31,6 +31,10 @@ const UserSchema = new Schema({
     type: String,
     default: "",
   },
+  service : {
+    type : String,
+    required : true
+  }
 });
 
 const User = mongoose.model("User", UserSchema);
